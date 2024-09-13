f6ebd783b460953236ee10631edcb1f41a253211
Antonio Aburto <aaburto583@gmail.com>
2024-09-13 15:28:11 -0500

    feat: ejercicio basico 3

---
+++ ejerciciobasico3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerciciobasico3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            char ascii;
+
+            Console.WriteLine("Ingrese valor: ");
+            ascii = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Valor ASCII de :" + ascii + " es " + Convert.ToInt32(ascii));
+            Console.ReadKey();
+
+
+        }
+    }
+}
